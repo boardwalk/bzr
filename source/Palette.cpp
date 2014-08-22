cdd8b114f14f93888f26515fd23737f45be7618a
Dan Skorupski <dan.skorupski@gmail.com>
2014-08-22 09:13:55 -0500

    Palettes are bgra, not rgba.

--- source/Palette.cpp
+++ source/Palette.cpp
@@ -3,28 +3,28 @@
 
 Palette::Palette(uint32_t id, const void* data, size_t size) : ResourceImpl(id)
 {
-	BlobReader reader(data, size);
+    BlobReader reader(data, size);
 
-	auto resourceId = reader.read<uint32_t>();
-	assert(resourceId == id);
-	assert((resourceId & 0xFF000000) == 0x04000000);
+    auto resourceId = reader.read<uint32_t>();
+    assert(resourceId == id);
+    assert((resourceId & 0xFF000000) == 0x04000000);
 
-	auto numColors = reader.read<uint32_t>();
-	assert(numColors == 2048);
-	_colors.resize(numColors);
+    auto numColors = reader.read<uint32_t>();
+    assert(numColors == 2048);
+    _colors.resize(numColors);
 
-	for(auto& color : _colors)
-	{
-		color.red = reader.read<uint8_t>();
-		color.green = reader.read<uint8_t>();
-		color.blue = reader.read<uint8_t>();
-		color.alpha = reader.read<uint8_t>();
-	}
+    for(auto& color : _colors)
+    {
+        color.blue = reader.read<uint8_t>();
+        color.green = reader.read<uint8_t>();
+        color.red = reader.read<uint8_t>();
+        color.alpha = reader.read<uint8_t>();
+    }
 
-	reader.assertEnd();
+    reader.assertEnd();
 }
 
 const vector<Palette::Color>& Palette::colors() const
 {
-	return _colors;
+    return _colors;
 }
