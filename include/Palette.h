9ca30552bb3b164bf886f219f6f08c6203ebacb5
Dan Skorupski <dan.skorupski@gmail.com>
2014-08-29 08:24:04 -0500

    Swap Palette::Color field order to match binary format.

--- include/Palette.h
+++ include/Palette.h
@@ -25,9 +25,9 @@ class Palette : public ResourceImpl<ResourceType::Palette>
 public:
     struct Color
     {
-        uint8_t red;
-        uint8_t green;
         uint8_t blue;
+        uint8_t green;
+        uint8_t red;
         uint8_t alpha;
     };
 
