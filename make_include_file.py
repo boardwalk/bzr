8afe6f5a8c35cdce0434d5a44f1c0e8f99e6bbc8
Dan Skorupski <dan.skorupski@gmail.com>
2014-09-10 22:14:08 -0500

    Compress shader text.

--- make_include_file.py
+++ make_include_file.py
@@ -3,20 +3,40 @@ import argparse
 import os
 import re
 
+def process_file(path):
+    with open(path) as inf:
+        content = inf.read()
+
+    # remove c-style comments
+    content = re.sub('/\*.+\*/', '', content, 0, re.DOTALL)
+
+    # remove cpp-style comments
+    content = re.sub('//.+', '', content)
+
+    # replace includes
+    def include_file(m):
+        return process_file(os.path.join('source', m.group(1)))
+
+    content = re.sub('^#include "([^"]+)"$', include_file, content, 0, re.MULTILINE)
+
+    # replace newlines with spaces
+    def replace_newline(m):
+        return m.group(1) + ('\n' if m.group(1).startswith('#') else ' ')
+
+    content = re.sub('(.*)\n', replace_newline, content)
+
+    # compress extra spaces
+    content = re.sub(' +', ' ', content)
+
+    # remove leading and trailing spaces
+    content = content.strip()
+
+    return content
+
 def escape_c_string(s):
     s = s.replace("\n", "\\n")
     s = s.replace("\"", "\\\"")
     return s
-
-def process_file(path, outf):
-    with open(path) as inf:
-        for ln in inf:
-            m = re.match('^#include "([^"]+)"$', ln)
-            if m:
-                include_path = os.path.join('source', m.group(1))
-                process_file(include_path, outf)
-            else:
-                outf.write("\n    \"{}\"".format(escape_c_string(ln)))
 
 parser = argparse.ArgumentParser()
 parser.add_argument('infile')
@@ -25,6 +45,6 @@ args = parser.parse_args()
 root, ext = os.path.splitext(args.infile)
 
 with open(args.outfile, 'w') as outf:
-    outf.write('static const char {}[] ='.format(os.path.basename(root)))
-    process_file(args.infile, outf)
-    outf.write(";\n")
+    varname = os.path.basename(root)
+    content = escape_c_string(process_file(args.infile))
+    outf.write('static const char {}[] = "{}";\n'.format(varname, content))
