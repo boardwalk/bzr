9730f563e9d8a90374efb2e25dda22f8a7d8fb4f
Dan Skorupski <dan.skorupski@gmail.com>
2014-10-07 17:21:12 -0500

    Use constants, idjit.

--- source/Region.cpp
+++ source/Region.cpp
@@ -32,7 +32,7 @@ static void readAlphaTex(BinReader& reader)
         assert(tcode == 8 || tcode == 9 || tcode == 10);
 
         uint32_t texId = reader.readInt();
-        assert((texId & 0xFF000000) == 0x05000000);
+        assert((texId & 0xFF000000) == ResourceType::kTextureLookup5);
     }
 }
 
@@ -46,7 +46,7 @@ static void read(BinReader& reader, Region::SceneType& sceneType)
     for(ResourcePtr& scene : sceneType.scenes)
     {
         uint32_t resourceId = reader.readInt();
-        assert((resourceId & 0xFF000000) == 0x12000000);
+        assert((resourceId & 0xFF000000) == ResourceType::kScene);
 
         scene = Core::get().resourceCache().get(resourceId);
     }
@@ -69,7 +69,7 @@ static void read(BinReader& reader, Region::TerrainType& terrainType)
 static void read(BinReader& reader, Region::TerrainTex& terrainTex)
 {
     terrainTex.resourceId = reader.readInt();
-    assert((terrainTex.resourceId & 0xFF000000) == 0x05000000);
+    assert((terrainTex.resourceId & 0xFF000000) == ResourceType::kTextureLookup5);
 
     /*uint32_t texTiling = */reader.readInt();
 
@@ -88,7 +88,7 @@ static void read(BinReader& reader, Region::TerrainTex& terrainTex)
     /*uint32_t detailTexTiling = */reader.readInt();
 
     uint32_t detailTexId = reader.readInt();
-    assert((detailTexId & 0xFF000000) == 0x05000000);
+    assert((detailTexId & 0xFF000000) == ResourceType::kTextureLookup5);
 }
 
 Region::Region(uint32_t id, const void* data, size_t size) : ResourceImpl{id}
